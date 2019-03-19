52ce27c7ef2c31b0bc04e377a88fc456835fad10
yli <shady831213@126.com>
2019-03-19 02:13:52 -0700

    add top doc

--- jarvism.go
+++ jarvism.go
@@ -1,3 +1,57 @@
+/*
+Just A Really Very Impressive Simulation Manager.
+
+Usage:
+
+	jarvism <command> [arguments]
+
+The commands are:
+
+	init        create a jarvism default project
+	run_parse   only parse cfg(jarvism_cfg dir or jarvism_cfg.yaml file)
+	run_test    run single test, build name must assigned
+	run_group   run group
+	run_build   run single build
+	show_tests  list tests in corresponding build
+	show_groups list all groups
+	show_builds list all builds
+	show_plugins list all plugins or reporter, simulator, runner, checker, testDiscoverer
+
+Use "jarvsim help <command>" for more information about a command.
+
+plugins:
+all runners:
+	 host
+all testDiscoverers:
+	 uvm_test
+all simulators:
+	 vcs
+all checkers:
+	 compileChecker
+	 testChecker
+all reporters:
+	 junit
+
+run options:
+
+  -compile_args
+    	compiling args pass to simulator (default false)
+  -max_job int
+    	limit of runtime coroutines, default is unlimited. (default -1)
+  -repeat
+    	run each testcase repeatly n times (default )
+  -reporter
+    	add reporter plugin, can apply multi times, default
+  -seed
+    	run testcase with specific seed
+  -sim_args
+    	simulation args pass to simulator (default false)
+  -sim_only
+    	bypass compile and only run simulation, default is false.
+  -unique
+    	if set jobId(timestamp) will be included in hash, then builds and testcases will have unique name and be in unique dir.default is false.
+*/
+
 package main
 
 import (
